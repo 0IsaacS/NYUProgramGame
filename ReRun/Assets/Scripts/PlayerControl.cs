2f99d29c61e7012e395725d8a0dc54301a2de7e9
TedIwaArdN <101580063+TedIwaArdN@users.noreply.github.com>
2022-07-21 11:07:06 -0400

    Add files via upload

--- ReRun/Assets/Scripts/PlayerControl.cs
+++ ReRun/Assets/Scripts/PlayerControl.cs
@@ -15,84 +15,67 @@ public class PlayerControl : MonoBehaviour
     public FireWeapon fireWeapon;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         facingRight = true;
         gameObject.layer = 1; //move this to another script later
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.D))
-        {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.D)) {
             isMovingRight = true;
-
+            
         }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
+        if (Input.GetKeyDown(KeyCode.A)) {
             isMovingLeft = true;
-
+            
         }
 
-        if (Input.GetKeyUp(KeyCode.D))
-        {
+        if (Input.GetKeyUp(KeyCode.D)) {
             isMovingRight = false;
             playerAnimator.SetBool("isWalking", false);
         }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
+        if (Input.GetKeyUp(KeyCode.A)) {
             isMovingLeft = false;
             playerAnimator.SetBool("isWalking", false);
         }
 
-        /*
-        if (fireWeapon.hasFired)
-        {
+        if (fireWeapon.hasFired) {
             isMovingLeft = isMovingRight = false;
         }
-        */
 
-        if (isMovingRight)
-        {
+        if (isMovingRight) {
             playerAnimator.SetBool("isWalking", true);
             move(Vector3.right);
-            if (!facingRight)
-            {
+            if (!facingRight) {
                 transform.Rotate(new Vector3(0, 180, 0));
                 facingRight = true;
             }
         }
-        else if (isMovingLeft)
-        {
+        else if (isMovingLeft) {
             playerAnimator.SetBool("isWalking", true);
             move(Vector3.right);
-            if (facingRight)
-            {
+            if (facingRight) {
                 transform.Rotate(new Vector3(0, 180, 0));
                 facingRight = false;
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && onGround)
-        {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround) {
             jump();
         }
     }
 
-    private void move(Vector3 dir)
-    {
+    private void move(Vector3 dir) {
         transform.Translate(dir * movingSpeed * Time.deltaTime);
     }
-    private void jump()
-    {
+    private void jump() {
         onGround = false;
         GetComponent<Rigidbody2D>().AddForce(Vector3.up * 10, ForceMode2D.Impulse);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "ground")
-        {
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == "ground") {
             onGround = true;
             GetComponent<Rigidbody2D>().AddForce(Vector3.up, ForceMode2D.Force);
         }
