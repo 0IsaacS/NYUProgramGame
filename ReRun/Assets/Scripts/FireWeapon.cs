aabbcccd89d26aab49316318d4e1f8a59176c1b6
TedIwaArdN <edwardtian619@gmail.com>
2022-07-21 11:45:39 -0400

    New

--- ReRun/Assets/Scripts/FireWeapon.cs
+++ ReRun/Assets/Scripts/FireWeapon.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class FireWeapon : MonoBehaviour
 {
     //Fields
-    public GameObject weaponPrefab;
+    public GameObject[] weaponPrefab;
     public Animator playerAnimator;
     [SerializeField] private Transform firePoint;
-    public PlayerControl player;
+    public PlayerMovement player;
     public bool hasFired;
 
     // Start is called before the first frame update
@@ -50,8 +50,9 @@ public class FireWeapon : MonoBehaviour
         playerAnimator.SetBool("isAttacking", true);
         hasFired = true;
         yield return new WaitForSeconds(0.1f);
-        Instantiate(weaponPrefab, firePoint.position, player.transform.rotation);
-        yield return new WaitForSeconds(waitLength - 0.1f);
+        int index = (int)Random.Range(0, weaponPrefab.Length);
+        Instantiate(weaponPrefab[index], firePoint.position, player.transform.rotation);
+        yield return new WaitForSeconds(waitLength);
         hasFired = false;
         playerAnimator.SetBool("isAttacking", false);
     }
